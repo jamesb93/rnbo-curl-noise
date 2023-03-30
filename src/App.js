082ae21aec2f874bb44dd006a776cda17b438c6f
James Bradbury <github@jamesbradbury.net>
2023-03-30 18:52:08 +0800

    hide controls

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ export default function App() {
     const [fov, setFov] = useState(200)
     const [curl, setCurl] = useState(0.25)
 
-    const props = useControls({
-        focus: { value: 5.1, min: 3, max: 7, step: 0.01 },
-        speed: { value: 100, min: 0.1, max: 100, step: 0.1 },
-        aperture: { value: 1.8, min: 1, max: 5.6, step: 0.1 },
-        fov: { value: 50, min: 0, max: 200 },
-        curl: { value: 0.25, min: 0.01, max: 0.5, step: 0.01 }
-    })
+    // const props = useControls({
+    //     focus: { value: 5.1, min: 3, max: 7, step: 0.01 },
+    //     speed: { value: 100, min: 0.1, max: 100, step: 0.1 },
+    //     aperture: { value: 1.8, min: 1, max: 5.6, step: 0.1 },
+    //     fov: { value: 50, min: 0, max: 200 },
+    //     curl: { value: 0.25, min: 0.01, max: 0.5, step: 0.01 }
+    // })
 
     useEffect(() => {
         async function initDevice() {
